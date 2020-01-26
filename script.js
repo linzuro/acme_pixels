90c25833656f1a2b96c1775818e13906d230fb1b
Lindsey Zurovchak <zurovchaklindsey@gmail.com>
2020-01-26 13:25:49 -0500

    final

--- script.js
+++ script.js
@@ -26,23 +26,45 @@ let button = document.querySelector('button');
 button.addEventListener('click',(ev)=>{
     let rowInput=parseInt(document.querySelector('[name="row"]').value)
     let columnInput=parseInt(document.querySelector('[name="column"]').value)
-    let table = document.querySelector('table')
+    let table = document.querySelector('tbody')
     ev.preventDefault();
+    table.innerHTML=""
     for (let i = 0; i < rowInput; i++){
         table.innerHTML += '<tr>'
-        console.log(table.innerHTML)
-
         for (let j = 0; j < columnInput; j++){
-            table.innerHTML += '<td></td>'     
-
+            let tr = document.querySelectorAll('tr')[i]
+            tr.innerHTML += '<td></td>'     
         }
-        table.innerHTML += '</tr>'
+        //table.innerHTML += '</tr>'
     }
 
+    table.style.width=rowInput*100+rowInput
+    let cells = [...document.querySelectorAll("td")]
+    cells.forEach((elem)=>{
+        elem.style.width="25%";
+    })
     
 
 })
 
-let colSize = document.querySelectorAll('td').length / document.querySelectorAll('tr').length
+let colorGrid = document.querySelector('.colorpicker')
+document.querySelectorAll('.colorblock')[0].classList.add("selected")
+colorGrid.addEventListener('click',(ev)=>{
+    const target=ev.target
+    const current = colorGrid.querySelector(".selected")
+    current.classList.remove('selected')
+    target.classList.add('selected')
+})
 
+let gridCells=document.querySelector("table")
+gridCells.addEventListener('click',(ev)=>{
+    const target=ev.target;
+    if(target.style.backgroundColor===colorGrid.querySelector(".selected").style.backgroundColor){
+        target.style.backgroundColor="#FFFFFF";
+    }else{
+        target.style.backgroundColor=colorGrid.querySelector(".selected").style.backgroundColor
+    }
+    
+})
+let colSize = document.querySelectorAll('td').length / document.querySelectorAll('tr').length
 let rowSize = document.querySelectorAll('tr').length
